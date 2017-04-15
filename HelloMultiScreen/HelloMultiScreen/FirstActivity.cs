8aef67a23d77376d3fdb4e27db4d60857026b29f
JGarnick <garnick.josh@gmail.com>
2017-04-15 13:11:30 -0700

    Added project go repo

---
+++ HelloMultiScreen/HelloMultiScreen/FirstActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace HelloMultiScreen
+{
+    [Activity(Label = "HelloMultiScreen", MainLauncher = true, Icon = "@drawable/icon")]
+    public class FirstActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            Intent second;
+            base.OnCreate(bundle);
+            //Use UI created in Main.axml
+            SetContentView(Resource.Layout.Main);
+            var pokeSecond = FindViewById<Button>(Resource.Id.pokeSecond);
+            pokeSecond.Click += (sender, e) => {
+                second = new Intent(this, typeof(SecondActivity));
+                second.PutExtra("Data", true);
+                StartActivity(second);
+            };
+
+            var hiToSecond = FindViewById<Button>(Resource.Id.hiToSecond);
+            hiToSecond.Click += (sender, e) => {
+                second = new Intent(this, typeof(SecondActivity));
+                second.PutExtra("Data", "Hi from screen 1!");
+                StartActivity(second);
+            };
+        }
+    }
+}
+
---
+++ HelloMultiScreen/HelloMultiScreen/SecondActivity.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HelloMultiScreen
+{
+    [Activity(Label = "SecondActivity", ParentActivity = typeof(FirstActivity))]
+    //[MetaData(NavUtils.ParentActivity, Value = ".FirstActivity")]
+    public class SecondActivity : Activity
+    {
+        int _counter;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Track number of times activity has been started
+
+            if (savedInstanceState != null)
+            {
+                _counter = savedInstanceState.GetInt("created_count", 0);
+            }
+            _counter++;
+            // Create your application here
+            SetContentView(Resource.Layout.Second);
+            ActionBar.SetDisplayShowHomeEnabled(true);
+
+            var label = FindViewById<TextView>(Resource.Id.screen2Label);
+            var firstData = Intent.GetBooleanExtra("Data", false);
+            
+            if (firstData == true)
+            {
+                label.Text = "Screen 1 poked you!";
+            }
+            else
+                label.Text = Intent.GetStringExtra("Data") ?? "Data not available";
+        }
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            outState.PutInt("created_count", _counter);
+            
+            base.OnSaveInstanceState(outState);
+        }
+
+        protected void OnPause(Bundle outState)
+        {
+            base.OnPause();
+            OnSaveInstanceState(outState);
+        }
+
+        protected void OnStop(Bundle outState)
+        {
+            base.OnStop();
+            OnSaveInstanceState(outState);
+        }
+    }
+}
